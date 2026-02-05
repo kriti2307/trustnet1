920a919e51fd50a1d318871998a6528d117a4c82
Kriti Saraogi <kritisaraogi@gmail.com>
2026-02-05 11:36:23 +0530

    nice

--- pages/forAuthors/guidelines.jsx
+++ pages/forAuthors/guidelines.jsx
@@ -118,8 +118,7 @@ function Guidelines() {
             </h2>
 
             <p className="text-gray-700 leading-relaxed text-justify">
-              Once an article is submitted to the conference, the authors agree
-              to our review policy.
+             Once an article is submitted to the conference, the authors agree to our review policy, where the article will be peer-reviewed by professional reviewers after being checked for similarity index and plagiarism by plagiarism detection software (Turnitin). ACIFF 2026 respects Intellectual Property Rights. Plagiarism is defined as the incorrect appropriation of someone else&apos;s information, thoughts, or ideas and presenting them as one&apos;s own. The conference committee will not tolerate this sort of breach of ethical norms at any cost. Any article with more than 18% similarity index will be rejected immediately without further consideration. The similarity should not be greater than 2% from a single source.
             </p>
 
             <p className="text-gray-700 leading-relaxed text-justify">
@@ -134,8 +133,7 @@ function Guidelines() {
             </h2>
 
             <p className="text-gray-700 leading-relaxed text-justify">
-              Authors submitting manuscripts acknowledge that their work has not
-              been previously published.
+              Our conference aims to present novel research and ensure efficient use of reviewers' efforts. Authors submitting manuscripts acknowledge that their work has not been previously published or accepted for publication in a substantially similar form in any peer-reviewed venue. Any breach of these conditions will result in rejection and notification to the other venue where the submission was made. Submissions should be original and not duplicate prior publications or ongoing submissions. Include citations in your manuscript if you need to reference another paper concurrently submitted to the conference. Clearly articulate in your paper how your submission differs significantly from these concurrent works and provide the additional papers in the supplemental material. Authors are encouraged to contact the Program Chairs for further clarification if needed.
             </p>
           </div>
 
@@ -145,8 +143,7 @@ function Guidelines() {
               Attendance Responsibilities
             </h2>
             <p>
-              If the paper is accepted, at least one of the authors must
-              register and present.
+             The authors must agree that if the paper is accepted, at least one of the authors will register for the conference and present the paper there. Acceptance of a paper does not claim for final publication. Only presented papers will be forwarded to the publication partner. Presenters will be provided with a certificate of paper presentation.
             </p>
           </div>
 
@@ -156,8 +153,9 @@ function Guidelines() {
               Publication
             </h2>
             <p>
-              Select accepted and presented papers will be submitted for
-              publication.
+              Select accepted and presented papers at the conference will be submitted for publication through our publication partners. Before final publication, proofs may be sent to the corresponding author designated in the manuscript. It is crucial to provide the corresponding author's correct email address. Please regularly visit the conference website for updates on publication specifics.
+
+Papers found to be plagiarised/having ethical concerns during any stage of the publication process (either by the conference committee or by the publication partner) shall be rejected immediately. If found in CRC of manuscript after registration fees deposition, then registration fees will NOT be refunded.
             </p>
           </div>
 
@@ -167,7 +165,7 @@ function Guidelines() {
               Publicity
             </h2>
             <p>
-              Articles cannot be publicised until official acceptance.
+              All articles submitted to the conference cannot be fully or partially publicised on any social media platform until an official acceptance email is received from the publisher. If any author(s) violate this act, their article will be rejected for presentation and removed from the proceeding for publication.
             </p>
           </div>
 
